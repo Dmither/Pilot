55a9209715b9ce4fc87c6c477346f56d4143984e
Dmither <mr.d.graumann@gmail.com>
2023-02-07 15:30:16 +0200

    add hero

--- js/script.js
+++ js/script.js
@@ -7,3 +7,32 @@ burger.addEventListener('click', function(){
   menu.classList.toggle('menu_active');
   body.classList.toggle('freeze');
 })
+
+new Swiper('.hero__categories', {
+  simulateTouth: true,
+  grabCursor: true,
+  keyboard: {           // управління клавиатурою
+    enabled: true,      // вкл/викл
+    onlyInViewport: true, // тільки в межах в'юпорта
+    pageUpDown: false,   // клавішами pageUp, pageDown
+  },
+  spaceBetween: 30,
+})
+
+new Swiper('.hero__footer', {
+  simulateTouth: false,
+  grabCursor: false,
+  keyboard: {           // управління клавиатурою
+    enabled: false,      // вкл/викл
+    onlyInViewport: true, // тільки в межах в'юпорта
+    pageUpDown: false,   // клавішами pageUp, pageDown
+  },
+  spaceBetween: 148,
+  slidesPerView:'auto',
+  loop: true,
+  autoplay: {           // автопрокрутка
+    delay: 1000,        // затримка
+    stopOnLastSlide: false,  // стоп на останньому
+    disableOnInteraction: false,  // викл після ручного перемикання
+  },
+});

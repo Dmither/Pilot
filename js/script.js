5062813ade825d4c9b43bfb4730b94f0377e911d
Dmither <mr.d.graumann@gmail.com>
2023-02-09 11:48:27 +0200

    add stories

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 let burger = document.querySelector('.burger');
 let menu = document.querySelector('.menu');
 let body = document.body;
-console.log(body.offsetWidth)
-console.log( body.offsetWidth / 1200 * 148)
 
 burger.addEventListener('click', function(){
   burger.classList.toggle('burger_active');
   menu.classList.toggle('menu_active');
   body.classList.toggle('freeze');
 })
+
+
+// hero sliders -----------------------------------------------------
 
 new Swiper('.hero__categories', {
   simulateTouth: true,
@@ -38,3 +39,56 @@ new Swiper('.hero__footer', {
     disableOnInteraction: false,  // викл після ручного перемикання
   },
 });
+
+
+// rate -------------------------------------------------------------
+
+let markArray = [1067, 211, 39, 21, 68];
+
+let marks = document.querySelectorAll('.stars__mark>span');
+let counts = document.querySelectorAll('.stars__count>span');
+let lines = document.querySelectorAll('.stars__line>span');
+let percents = document.querySelectorAll('.stars__line>div');
+
+let generalMark = document.querySelector('.rate__mark');
+let generalReviews = document.querySelector('.rate__reviews')
+
+window.addEventListener('load', function(){
+  let points = 0;
+  let reviews = 0;
+  for (let i = 0; i < marks.length; i++){
+    // culc points and reviews
+    counts[i].textContent = new Intl.NumberFormat('en-IN').format(markArray[i]);
+    points += (Number(marks[i].textContent) * markArray[i]);
+    reviews += Number(markArray[i]);
+    
+  }
+  for (let i = 0; i < marks.length; i++){
+    // set line width and persents
+    lines[i].style.width = String(Number(markArray[i]) / reviews * 100) + '%'
+    percents[i].textContent = String((markArray[i] / reviews * 100).toFixed(0)) + '%'
+  }
+  // set general mark and total reviews count
+  generalMark.textContent = (points / reviews).toFixed(1)
+  generalReviews.textContent = (new Intl.NumberFormat('en-IN').format(reviews));
+})
+
+
+// stiories slider ---------------------------------------------------
+
+new Swiper('.stories__reviews', {
+  navigation: {
+    nextEl: '.stories__right',
+    prevEl: '.stories__left',
+  },
+  keyboard: {           // управління клавиатурою
+    enabled: true,      // вкл/викл
+    onlyInViewport: true, // тільки в межах в'юпорта
+    pageUpDown: false,   // клавішами pageUp, pageDown
+  },
+  simulateTouth: false,
+  autoHeight: true,
+  slidesPerView: 'auto',
+  watchOverflow: true,
+  spaceBetween: 23,
+})
